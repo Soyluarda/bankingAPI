b7f923b927f60582a47f1ae1abb3742db0fbb1cf
Arda Soylu <ardasoylu39@hotmail.com>
2021-02-08 02:05:23 +0300

    Remove unnecessary credentials

--- bankingAPI/settings/stage.py
+++ bankingAPI/settings/stage.py
@@ -25,18 +25,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
 }
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dfo6283p92kijd',
-        'USER': 'yllgynwycuhyaj',
-        'PASSWORD': 'b9d1bd048d81b97e7c8cf34663c8db6640b35b5fc2ddc253f00b14ac58982ba8',
-        'HOST': 'ec2-34-200-116-132.compute-1.amazonaws.com',
-        'PORT': '5432'
-    }
-}
-"""
 
 def show_toolbar(request):
     return False
